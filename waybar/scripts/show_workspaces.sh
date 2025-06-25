35be13b2288a470cfa3e7bd0c7e52c8e3df0e141
Iskandar <silvapaco810@gmail.com>
2025-06-24 23:41:05 -0600

    [UPDATE]: Se ajusto el icono central (Waybar)

--- waybar/scripts/show_workspaces.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-selected=$(wofi --show dmenu --prompt "Selecciona escritorio" < <(hyprctl workspaces -j | jq -r '.[].id'))
-
-if [[ "$selected" =~ ^[0-9]+$ ]]; then
-  hyprctl dispatch workspace "$selected"
-fi
--- waybar/scripts/toggle_drawer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Busca si ya está corriendo
-if pgrep -f "waybar.*config-drawer.jsonc" > /dev/null; then
-  pkill -f "waybar.*config-drawer.jsonc"
-else
-  waybar -c ~/.config/waybar/config-drawer.jsonc -s ~/.config/waybar/style.css &
-fi
